e4078c3c086577b784cf2fa401a6dbe9ce835f38
shubham-available <shubhamk314@gmail.com>
2023-03-22 19:58:15 +0530

    episode-2

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # namaste-react
+
+# Parcel
+
+- Dev Build
+- Creates Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs support
+- Tree Shaking - remove unused code
+- Different dev & prod bundles
+<!-- remove main:"index.js" from package.json file -->
--- index.js
+++ index.js
@@ -1,18 +1,69 @@
-// Here React.createElement() is just a object which will be turned to html tag by the render method by creating a tree in the dom.
-// ReactElement(Object) => HTML(Browser Understands)
-// Whenever we pass root.render, the contents in div.#root will be replaced by that.
-// We can use react in uor existing project because it wil only affect only the root html tag. That's why its a library not a framework.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {}, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// React.createElement => Object => HTML(DOM)
+
+// const parent = React.createElement("div", {}, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement(
+//       "h1",
+//       {
+//         key: "h1",
+//       },
+//       "I'm an h1 tag"
+//     ),
+//     React.createElement(
+//       "h2",
+//       {
+//         key: "h2",
+//       },
+//       "I'm an h2 tag"
+//     ),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement(
+//       "h1",
+//       {
+//         key: "h1",
+//       },
+//       "I'm an h1 tag"
+//     ),
+//     React.createElement(
+//       "h2",
+//       {
+//         key: "h2",
+//       },
+//       "I'm an h2 tag"
+//     ),
+//   ]),
+// ]);
+
+// JSX =>  React.createElement => Object => HTML(DOM): under the hood babel is working converting this to html(dom)
+// JSX always sanitizes the code so that no malicious code is being rendered.
+
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+
+// React Component
+// - Functional Component
+// - Class Based Component
+
+// Composition components means passing components into other components.
+
+function App() {
+  return (
+    <div>
+      {Title()}
+      <h1 id="title" key="h2">
+        Namaste React functional component
+      </h1>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(parent);
+root.render(<App />);
