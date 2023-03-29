1a8d68fbaa649f5c1f7bc6089140f97f6a6ce533
webdevexpress <business.webdevexpress@gmail.com>
2023-03-29 13:16:15 +0530

    episode-4

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+function Title() {
+  return (
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://yt3.googleusercontent.com/ytc/AL5GRJXudT76175T4x4n7eslWM1YkgNLHDSSqfXGoadl=s900-c-k-c0x00ffffff-no-rj"
+      />
+    </a>
+  );
+}
+
+export {Title};
